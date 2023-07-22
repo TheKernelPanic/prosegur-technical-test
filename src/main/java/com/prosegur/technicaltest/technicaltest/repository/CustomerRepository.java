61d4dfeeaef215817fd823f036ca8403a95077de
Kernelpanic <kernelpanic0x000007c4@protonmail.com>
2023-07-23 01:45:03 +0200

    Customer repository contract.

--- src/main/java/com/prosegur/technicaltest/technicaltest/repository/CustomerRepository.java
+++ src/main/java/com/prosegur/technicaltest/technicaltest/repository/CustomerRepository.java
@@ -4,6 +4,9 @@ import com.prosegur.technicaltest.technicaltest.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findByDni(String dni);
 }
