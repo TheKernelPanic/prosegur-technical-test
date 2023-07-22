9f33ec5daf0c8b7ceb438165e158e58a4fcf821d
Kernelpanic <kernelpanic0x000007c4@protonmail.com>
2023-07-23 01:45:39 +0200

    Score dto for service response.

---
+++ src/main/java/com/prosegur/technicaltest/technicaltest/dto/ScoreDto.java
@@ -0,0 +1,13 @@
+package com.prosegur.technicaltest.technicaltest.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ScoreDto {
+
+    @JsonProperty("score")
+    private double score;
+}
